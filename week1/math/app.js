612d343a0d0b6ddf6b1b855cffb62e2e840cadb2
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-07-31 16:49:14 -0500

    first commit

---
+++ week1/math/app.js
@@ -0,0 +1,7 @@
+console.log(require("./math.js").sum(10, 15))
+
+console.log(require("./math.js").subtract(10, 5))
+
+console.log(require("./math.js").multiply(8, 4))
+
+console.log(require("./math.js").divide(10, 2))
---
+++ week1/math/math.js
@@ -0,0 +1,15 @@
+module.exports.sum = function(a, b){
+    return a + b
+}
+
+module.exports.subtract = function(a, b){
+    return a - b
+}
+
+module.exports.multiply = function(a, b){
+    return a * b
+}
+
+module.exports.divide = function(a, b){
+    return a / b
+}
